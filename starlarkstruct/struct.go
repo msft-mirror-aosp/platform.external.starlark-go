50ef618d22b6272de637b3caf646baf667f66421
Mikhail Mazurskiy <126021+ash2k@users.noreply.github.com>
2026-09-08 18:35:44 +0100

    Update Struct.AttrAt to return name and value (#657)

--- starlarkstruct/struct.go
+++ starlarkstruct/struct.go
@@ -247,16 +247,9 @@ func (s *Struct) AttrNames() []string {
 }
 
 // AttrAt returns the value of the field at the specified index.
-func (s *Struct) AttrAt(i int) (starlark.Value, error) {
-	if !(0 <= i && i < len(s.entries)) {
-		var ctor string
-		if s.constructor != Default {
-			ctor = s.constructor.String() + " "
-		}
-		return nil, starlark.NoSuchAttrError(
-			fmt.Sprintf("%sstruct has no attribute at index %d", ctor, i))
-	}
-	return s.entries[i].value, nil
+func (s *Struct) AttrAt(i int) (string, starlark.Value) {
+	e := s.entries[i]
+	return e.name, e.value
 }
 
 func (x *Struct) CompareSameType(op syntax.Token, y_ starlark.Value, depth int) (bool, error) {
